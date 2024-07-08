66a1b62593eebb31337aaacb9795876d68460cc7
sdhadavai <sdhadavai.qa@gmail.com>
2024-07-08 12:23:36 +0530

    Updated review comments as per TL

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -5,9 +5,18 @@ import org.openqa.selenium.By;
 public class DemoPage {
 	
 	By demo = By.id("demo");
+	By cart = By.id("cart");
 	
 	public void getDemo() {
 		System.out.println("demo");
+		System.out.println("demo detils");
+	}
+	
+	public void addToCart() {
+		
+		System.out.println("cart");
+		System.out.println("cart detils");
+		
 	}
 
 }
