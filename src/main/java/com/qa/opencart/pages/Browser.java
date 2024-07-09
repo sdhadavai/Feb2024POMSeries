4f4e362e89f235c55731c18c790cc6ffdedeb420
sdhadavai <sdhadavai.qa@gmail.com>
2024-07-09 23:30:11 +0530

    Adding f1 feature

--- src/main/java/com/qa/opencart/pages/Browser.java
+++ src/main/java/com/qa/opencart/pages/Browser.java
@@ -4,8 +4,10 @@ public class Browser {
 	int p =10;
 	
 	public void loing() {
-		System.out.println("Tom login");
-		System.out.println("Tom Chrome");
+
+		System.out.println("Srini login");
+		System.out.println("Done!");
+
 	}
 
 }
---
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class Feature {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("f1 feature");
+		
+	}
+
+}
