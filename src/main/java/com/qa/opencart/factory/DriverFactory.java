ece42597a7e48cbbd8cad1d1305923d62aca8085
sdhadavai <sdhadavai.qa@gmail.com>
2024-07-11 14:16:02 +0530

    Updated Driver Factory getScreenshot method

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -144,19 +144,39 @@ public class DriverFactory {
 	 * @return 
 	 */
 	
-	public static String getScreenshot(String methodName) {
-		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);//temp location
-		String path = System.getProperty("user.dir") + "\\screenshots\\" + methodName + "_" + System.currentTimeMillis() + ".png";
-		File destination = new File(path);
-		try {
-			FileHandler.copy(srcFile, destination);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 
-		return path;
+public static String getScreenshot(String methodName) {
 		
+		// Get the driver instance
+        TakesScreenshot screenshotTaker = (TakesScreenshot) getDriver();
+        
+        // Take the screenshot and save it to a temporary location
+        File srcFile = screenshotTaker.getScreenshotAs(OutputType.FILE);
+        
+        // Define the path for the screenshots folder
+        String screenshotsDirPath = System.getProperty("user.dir") + "/screenshots";
+        
+        // Create the screenshots folder if it doesn't exist
+        File screenshotsDir = new File(screenshotsDirPath);
+        if (!screenshotsDir.exists()) {
+            if (screenshotsDir.mkdirs()) {
+                System.out.println("Folder 'screenshots' created successfully at: " + screenshotsDirPath);
+            } else {
+                System.out.println("Failed to create the folder 'screenshots' at: " + screenshotsDirPath);
+            }
+        }
+
+        // Define the destination path for the screenshot
+        String screenshotPath = screenshotsDirPath + "/" + methodName + "_" + System.currentTimeMillis() + ".png";
+        File destination = new File(screenshotPath);
+
+        // Copy the screenshot to the destination path
+        try {
+            FileHandler.copy(srcFile, destination);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return destination.getAbsolutePath();
 	}
 
 }
