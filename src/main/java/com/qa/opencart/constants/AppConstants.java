84d749b99014c6c067f7761855763f6add195edf
sdhadavai <sdhadavai.qa@gmail.com>
2024-07-11 14:23:48 +0530

    Upated LoginPage Title to check screenshots in Jenkins

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -11,7 +11,7 @@ public class AppConstants {
 	public static final String CONFIG_UAT_FILE_PATH = "./src/test/resources/config/uat.properties";
 	public static final String CONFIG_STAGE_FILE_PATH = "./src/test/resources/config/stage.properties";
 	
-	public static final String LOGIN_PAGE_TITLE ="Account Login";
+	public static final String LOGIN_PAGE_TITLE ="Account Login11";
 	public static final String LOGIN_PAGE_FRACTION_URL ="route=account/login";
 	public static final String ACCOUNT_PAGE_TITLE ="My Account";
 	public static final String ACCOUNT_PAGE_FRACTION_URL ="route=account/account";
